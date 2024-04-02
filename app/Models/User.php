5bfc0a4c3a2831209278ff1798e136cf6f92272e
GhadaKhamisM <ghada.khamis@ibtikar.net.sa>
2024-04-02 20:46:45 +0200

    Install pakages
    1) laravel/sanctum for authentication.
    2) laravel/telescope for debug App.

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'image',
     ];
 
     /**
@@ -40,7 +41,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
